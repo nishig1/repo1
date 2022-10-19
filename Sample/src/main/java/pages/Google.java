5e4d5b340a4562311b57d313b88951a9728b915e
venkatgn <venkatgn.gutta@gmail.com>
2022-10-19 17:31:50 +0530

    1st time commitig

---
+++ Sample/src/main/java/pages/Google.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Google 
+{
+  public void search()
+  {
+	  WebDriverManager.chromedriver().setup();
+	  ChromeDriver driver=new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.get("http://www.google.com");
+	  driver.findElement(By.name("q")).sendKeys("java");
+	  driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+  }
+}
---
+++ Sample/src/test/java/retest/Google_testing.java
@@ -0,0 +1,15 @@
+package retest;
+import org.testng.annotations.Test;
+
+import pages.Google;
+
+public class Google_testing 
+{
+	
+   @Test
+	public void smoketest()
+	{
+		Google google=new Google();
+		google.search();
+	}
+}
